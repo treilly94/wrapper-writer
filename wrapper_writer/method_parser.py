4837db129e5bfc54efa6ee54765994691fec31e6
treilly94 <treilly94@live.com>
2018-08-13 14:05:58 +0100

    Added a parse method function to the method parser

--- wrapper_writer/method_parser.py
+++ wrapper_writer/method_parser.py
@@ -5,3 +5,10 @@ class MethodParser:
     method_params = ""
     method_docs = ""
     method_returns = ""
+
+    def __init__(self, method_string):
+        self.method = method_string
+        self.parse_method()
+
+    def parse_method(self):
+        """This method parses the target method as a string into its individual components"""
