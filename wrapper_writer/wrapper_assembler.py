7fe336749496a8a257b99ef993e35f8685dc6024
treilly94 <treilly94@live.com>
2018-08-13 16:12:49 +0100

    TG-9 Got simple template working

--- wrapper_writer/wrapper_assembler.py
+++ wrapper_writer/wrapper_assembler.py
@@ -1,2 +1,11 @@
-def wrapper_assembler():
+import jinja2
+
+
+def wrapper_assembler(method):
     """A method that assembles the wrapper from a method object and a wrapper config"""
+
+    # Get and render the template
+    template_loader = jinja2.FileSystemLoader(searchpath="./templates/")
+    template_env = jinja2.Environment(loader=template_loader)
+    template = template_env.get_template("python.txt")
+    return template.render(name=method.method_name, param=method.method_params)
