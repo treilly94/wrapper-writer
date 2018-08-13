d8e63fbe7c86672e1f4d929eb79e6815ffb135c4
treilly94 <treilly94@live.com>
2018-08-13 14:52:50 +0100

    Parsed a simple scala function

--- wrapper_writer/method_parser.py
+++ wrapper_writer/method_parser.py
@@ -1,3 +1,5 @@
+import re
+
 class MethodParser:
     """A class that parses the details of each method"""
 
@@ -12,3 +14,21 @@ class MethodParser:
 
     def parse_method(self):
         """This method parses the target method as a string into its individual components"""
+        pattern = r'def (\w+)\((.*)\): (\w+)'
+        searched = re.search(pattern, self.method)
+        self.method_name = searched.group(1)
+        self.method_params = self.parse_params(searched.group(2))
+        self.method_returns = searched.group(3)
+
+    @staticmethod
+    def parse_params(params):
+        """This method splits the parameters and places them in the correct format"""
+        new_params = []
+        # Remove whitespace and split parameters by a comma
+        params = params.replace(" ", "").split(",")
+
+        for p in params:
+            p = p.split(":")
+            new_params.append((p[0], p[1]))
+
+        return new_params
