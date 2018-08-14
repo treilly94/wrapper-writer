a543de8f013e3a04f82f8b5b9826e10d33e15a96
treilly94 <treilly94@live.com>
2018-08-14 14:14:16 +0100

    Combined into one class

--- wrapper_writer/wrapper_assembler.py
+++ wrapper_writer/wrapper_assembler.py
@@ -1,11 +1,23 @@
 import jinja2
+import yaml
 
 
-def wrapper_assembler(templates_dir, template, method):
-    """A method that assembles the wrapper from a method object and a wrapper config"""
+class WrapperWriter:
+    templates_dir = ""
+    config = {}
 
-    # Get and render the template
-    template_loader = jinja2.FileSystemLoader(searchpath=templates_dir)
-    template_env = jinja2.Environment(loader=template_loader)
-    template = template_env.get_template(template)
-    return template.render(method=method)
+    def __init__(self, templates_dir):
+        self.templates_dir = templates_dir
+
+    def read_from_config(self, config):
+        """This method reads the details of the method from a yaml config file"""
+        self.config = yaml.load(open(config))
+
+    def wrapper_assembler(self, template):
+        """A method that assembles the wrapper from a method object and a wrapper config"""
+
+        # Get and render the template
+        template_loader = jinja2.FileSystemLoader(searchpath=self.templates_dir)
+        template_env = jinja2.Environment(loader=template_loader)
+        template = template_env.get_template(template)
+        return template.render(method=self.config)
