583b97bb40cd24a75a18764c9a77dc1734a55506
treilly94 <treilly94@live.com>
2018-08-16 10:44:10 +0100

    Updated read yaml test

--- tests/test_wrapper_writer.py
+++ tests/test_wrapper_writer.py
@@ -8,25 +8,34 @@ class TestWrapperWriter(TestCase):
     method = WrapperWriter("./tests/resources/config/")
 
     def test_read_yaml(self):
-        output = self.method.read_yaml("./tests/resources/config/config.yml")
+        self.method.read_yaml("./tests/resources/config/config.yml")
+        expected_structure = {'python': {'file_extension': '.py',
+                                         'path': '/test_dir/python/path/',
+                                         'template': 'python.txt'},
+                              'scala': {'file_extension': '.scala',
+                                        'path': '/test_dir/scala/path/',
+                                        'template': 'scala.txt'}
+                              }
+        expected_methods = {'testFunc': {'docs': 'A cool function',
+                                         'params': {'param1': 'String'},
+                                         'returns': 'String'}
+                            }
 
-        self.assertEqual("testFunc", output["name"])
-        self.assertEqual({"param1": "String"}, output["params"])
-        self.assertEqual("A cool function", output["docs"])
-        self.assertEqual("String", output["returns"])
+        self.assertEqual(expected_structure, self.method.structure)
+        self.assertEqual(expected_methods, self.method.methods)
 
     def test_get_project_root(self):
         pass
 
     def test_wrapper_assembler_python(self):
-        self.method.method_details = {
+        self.method.methods = {
             "name": "testFunc",
             "params": {"param1": "String"},
             "returns": "String",
             "docs": "A cool function"
         }
 
-        output = self.method.wrapper_assembler("python.txt")
+        output = self.method.wrapper_assembler("testFunc", "python.txt")
 
         with open('./tests/resources/expected/python.txt', 'r') as expected_file:
             expected = expected_file.read()
@@ -34,14 +43,14 @@ class TestWrapperWriter(TestCase):
         self.assertEqual(expected, output)
 
     def test_wrapper_assembler_scala(self):
-        self.method.method_details = {
+        self.method.methods = {
             "name": "testFunc",
             "params": {"param1": "String"},
             "returns": "String",
             "docs": None
         }
 
-        output = self.method.wrapper_assembler("scala.txt")
+        output = self.method.wrapper_assembler("testFunc", "scala.txt")
 
         with open('./tests/resources/expected/scala_without_docs.txt', 'r') as expected_file:
             expected = expected_file.read()
