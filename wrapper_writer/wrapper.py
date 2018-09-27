3aede7d5127b78ee47562dcf0bfa6c63df886f7d
treilly94 <treilly94@live.com>
2018-09-27 16:55:10 +0100

    Added write file method

--- wrapper_writer/wrapper.py
+++ wrapper_writer/wrapper.py
@@ -38,4 +38,14 @@ class Wrapper:
         pass
 
     def write_file(self):
-        pass
+        """This method populates the template, creates the appropriate file name, and writes the file"""
+        # Get completed template
+        output = self.populate_template()
+
+        # Get file name
+        file_name = self.create_file_name()
+
+        # Write file
+        file = open(file_name, "w")
+        file.write(output)
+        file.close()
