722b7fa8194535658918f9ae0a1f3f380f2aef7a
treilly94 <treilly94@live.com>
2018-09-27 16:10:51 +0100

    Created structure of the wrapper class

---
+++ wrapper_writer/wrapper.py
@@ -0,0 +1,14 @@
+class Wrapper:
+
+    def __init__(self, method, structure):
+        self.method = method
+        self.structure = structure
+
+    def populate_template(self):
+        pass
+
+    def generate_file_name(self):
+        pass
+
+    def write_file(self):
+        pass
