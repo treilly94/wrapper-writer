96ca57b55b7698e9b8a95fbf6abd6a833f0479e2
treilly94 <treilly94@live.com>
2018-09-28 14:03:34 +0100

    Added some more comments

--- wrapper_writer/container.py
+++ wrapper_writer/container.py
@@ -6,14 +6,21 @@ class Container:
 
     def create_config(self):
         """This method formats the details of the container and its methods into a yml syntax"""
+        # Set container name
         config = "%s:\n" % self.name
+        # Loop through methods
         for m in self.methods:
+            # Method name
             config += "\t%s:\n" % m.name
+            # Method params
             config += "\t\tparams:\n"
             for k, v in m.params.items():
                 config += "\t\t\t%s: %s\n" % (k, v)
+            # Method docs
             config += "\t\tdocs: %s\n" % m.docs
+            # Method returns
             config += "\t\treturns: %s\n" % m.returns
+            # Method other
             config += "\t\tother:\n"
             for k, v in m.other.items():
                 config += "\t\t\t%s: %s\n" % (k, v)
