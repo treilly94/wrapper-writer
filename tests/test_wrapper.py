f5fea99eb5a570613ef50db6646f5a91e3b630cc
treilly94 <treilly94@live.com>
2018-10-01 13:42:25 +0100

    Added test for no template

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -8,15 +8,16 @@ from wrapper_writer.wrapper import Wrapper
 
 # This test must be run with the working directory set as the project root
 class TestWrapper(TestCase):
-    m1 = Method(name="test_func",
-                params={"param1": "String"},
-                returns="String",
-                docs="A cool function",
-                other={})
+    def setUp(self):
+        m1 = Method(name="test_func",
+                    params={"param1": "String"},
+                    returns="String",
+                    docs="A cool function",
+                    other={})
 
-    container = Container(name="test_container", path="", methods=[m1])
-    structure = Structure(path="", template="testTemplate.scala.j2", file_name_format="prefix_%s.txt")
-    wrapper = Wrapper(project_root="./tests/resources/config/", container=container, structure=structure)
+        container = Container(name="test_container", path="", methods=[m1])
+        structure = Structure(path="", template="testTemplate.scala.j2", file_name_format="prefix_%s.txt")
+        self.wrapper = Wrapper(project_root="./tests/resources/config/", container=container, structure=structure)
 
     def test_populate_template(self):
         output = self.wrapper.populate_template()
@@ -25,6 +26,11 @@ class TestWrapper(TestCase):
             expected = expected_file.read()
 
         self.assertEqual(expected, output)
+
+    def test_populate_template_no_template(self):
+        self.wrapper.structure.template = "cats.txt"
+        with self.assertRaises(FileNotFoundError):
+            self.wrapper.populate_template()
 
     def test_create_file_name(self):
         expected = "prefix_test_container.txt"
