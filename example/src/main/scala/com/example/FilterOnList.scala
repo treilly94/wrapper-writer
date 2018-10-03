f73337b65b6d58136f8859eaeacfe9e2d871a8ac
bellis.kayleigh@gmail.com <bellis.kayleigh@gmail.com>
2018-10-03 16:09:47 +0100

    changing templates

--- example/src/main/scala/com/example/FilterOnList.scala
+++ example/src/main/scala/com/example/FilterOnList.scala
@@ -6,16 +6,25 @@ import org.apache.spark.sql.functions.col
 object FilterOnList {
 
   /**
-    * This function takes in a DataFrame
-    * @param df
-    * @param targetCol
-    * @param values
-    * @return
+    * This function calls a protected function which filters the data based on where the targetCol doesn't have values
+    * that are in the values parameter.
+    * @param df DataFrame - Stores all the data.
+    * @param targetCol String - Column to be filtered on.
+    * @param values List[Int] - List of values to compared.
+    * @return DataFrame
     */
   def filterOnList(df: DataFrame, targetCol: String, values: List[Int]): DataFrame = {
     filterFunct(df, targetCol, values)
   }
 
+  /**
+    * This function will take in a DataFrame and filter the data based on where the targetCol doesn't have values
+    * that are in the values parameter.
+    * @param df DataFrame - Stores all the data.
+    * @param targetCol String - Column to be filtered on.
+    * @param values List[Int] - List of values to compared.
+    * @return DataFrame
+    */
   protected def filterFunct(df: DataFrame, targetCol: String, values: List[Int]): DataFrame = {
     df.where(!col(targetCol).isin(values: _*))
   }
