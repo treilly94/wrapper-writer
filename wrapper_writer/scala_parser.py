dc1b0465eeac56b9b98586d65070fa714f669c3d
Ian Edwards <ian.edwards@qa.com>
2018-10-08 17:22:53 +0100

    Scala Parser, UML Alignment

--- wrapper_writer/scala_parser.py
+++ wrapper_writer/scala_parser.py
@@ -3,11 +3,12 @@ import yaml
 import os
 import glob
 import sys
-from wrapper_writer.container import Container
-from wrapper_writer.method import Method
+from container import Container
+from method import Method
 
 
 class ScalaParse:
+    container_classes = []
     """
     This ScalaParse class parses a scala file, extracts the method elements and writes them out to a config file
     :param filename: The name of the file to parse
@@ -85,6 +86,20 @@ class ScalaParse:
             dict_by_comma[k] = v.lstrip()
             new_dict = {k.lstrip(): v for k, v in dict_by_comma.items()}
         return new_dict
+    """The Method Class contains the details associated with a particular method.
+
+    :param name: The name of the method.
+    :type name: str
+    :param params: A dictionary where the keys are the methods parameter names and the values are the types.
+    :type params: dict
+    :param docs: The docstring of the method.
+    :type docs: str
+    :param returns: The return type of the object.
+    :type returns: str
+    :param other: A dictionary containing any additional values that may be required in the template.
+    :type other: dict
+    """
+    # def __init__(self, name, params, docs, returns, other={}):
 
     def multi_process(self):
         """
@@ -95,16 +110,25 @@ class ScalaParse:
         matches = tuple(all_found)
         if not matches:
             raise Exception("No Methods Found")
+        container_methods = []
         for i in matches:
             ig = i.group()
             base_raw = os.path.basename(self.filename)
             container_name = os.path.splitext(base_raw)[0]
             return_type = self.extract_return_type(ig)
             method_name = self.extract_method_name(ig)
+            other = {"other": "Other Stuff"}
+            dummy_docs = "This is a doc string"
             params = self.extract_params(ig)
-            data = {container_name: {method_name: {"params": params, "returns": return_type}}}
-            with open(self.config_filename, 'a') as yaml_file:
-                yaml.dump(data, yaml_file, default_flow_style=False)
+            one_method = Method(method_name, params, dummy_docs, return_type)
+            container_methods.append(one_method)
+            # data = {container_name: {method_name: {"params": params, "returns": return_type}}}
+            # with open(self.config_filename, 'a') as yaml_file:
+            #     yaml.dump(data, yaml_file, default_flow_style=False)
+        one_container = Container(container_name, container_methods)
+        cc = one_container.create_config()
+        print(cc)
+        # self.container_classes.append(one_container)
 
 
 class App:
@@ -114,7 +138,6 @@ class App:
     :param file: String object if the file to be parsed
     :param file_extension: String object of file extension to look for
     """
-
 
     def __init__(self, folder=None, logic_file=None, file_extension="*.scala", config_name="config_sadhg.yml", append_config=False):
         self.folder = folder
@@ -161,7 +184,6 @@ class App:
         else:
             raise Exception("Please provide either a file or folder, not both")
 
-
     def run_scala(self):
         scala_files_to_run = self.prepare_input()
         for p in scala_files_to_run:
@@ -169,5 +191,9 @@ class App:
             x.multi_process()
 
 
-
+if __name__ == '__main__':
+    t = App(logic_file="FilterOnList.scala", append_config=False, config_name="config_yesterdayrl.yml")
+    # t = App(logic_file="C:\Users\Ian Edwards\projects\dap-s\wrapper-writer\wrapper-writer\tests\resources\input\FilterOnList.scala")
+    # t = App(logic_file="scalacode.scala")
+    t.run_scala()
 
