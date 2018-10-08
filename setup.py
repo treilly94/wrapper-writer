d5c6e245cf08b5f9dfab3afa6fe43b9a3242ca42
treilly94 <treilly94@live.com>
2018-10-08 16:31:18 +0100

    Updated the setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='wrapper_writer',
     version='0.4',
-    author="Tom Reilly",
+    author="Tom Reilly, Kayleigh Bellis, Ian Edward",
     url="https://github.com/treilly94/wrapper-writer",
+    description=('Wrapper Writer aims to make the creation of repetitive files '
+                 'easier using templates and simple configuration files.'),
+    license='MIT',
     packages=find_packages(),
     install_requires=[
         "Jinja2",
